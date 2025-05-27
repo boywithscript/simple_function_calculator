4b3594c77889884f0904bfc4a3a4b4d46cf334ea
boywithscript_ <boywithscript@gmail.com>
2025-05-28 05:27:48 +0800

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,1 +1,15 @@
-this code
+# Function Calculator
+
+def cal(a, b, operation):
+    match operation:
+        case "+": return a + b
+        case "-": return a - b
+        case "*": return a * b
+        case "**": return a ** b
+        case "/": return a / b if b != 0 else  "Error: Can't divide by zero"
+        case _: return("Error : Operation invalid")
+       
+print (cal(2, 6, "+"))
+print (cal(6, 2, "/"))
+print (cal(2, 0, "/"))
+print (cal(3, 4, "%"))
